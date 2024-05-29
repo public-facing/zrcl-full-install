c539151de077915180e9a3b477557f26ef63d6a3
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-05-28 18:22:43 -0700

    feat(zrcl4.hashlib): add hash_folder

--- src/zrcl4/hashlib.py
+++ src/zrcl4/hashlib.py
@@ -1,7 +1,8 @@
 import hashlib
 import io
 from typing import overload
-
+import os
+import typing
 
 @overload
 def hash_file(file: str, algorithm="sha256", chunk_size=4096): ...
@@ -25,3 +26,19 @@ def hash_file(file, algorithm="sha256", chunk_size=4096):
 def hash_bytes(data, algorithm="sha256"):
     """Hash bytes with the specified algorithm."""
     return hashlib.new(algorithm, data).hexdigest()
+
+def hash_folder(path: str, allowed_extensions: typing.List[str] = None, algorithm="sha256", chunk_size=4096):
+    """Hash all files in a folder with the specified algorithm and return a dictionary of file hashes."""
+    folder_hashes = {}
+    collective_hash = hashlib.new(algorithm)
+    for root, _, files in os.walk(path):
+        for file in files:
+            if allowed_extensions and not file.lower().endswith(tuple(allowed_extensions)):
+                continue
+            file_path = os.path.join(root, file)
+            file_hash = hash_file(file_path, algorithm, chunk_size)
+            collective_hash.update(file_hash.encode("utf-8"))
+            folder_hashes[file_path] = file_hash
+    return folder_hashes, collective_hash.hexdigest()
+
+
